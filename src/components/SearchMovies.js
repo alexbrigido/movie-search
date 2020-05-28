f3aa58257386ca78d81e705b790fb0925dd4155a
Alexandre Dias <alexbrigido@hotmail.com>
2020-05-28 11:35:27 -0300

    api the movies db

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -2,8 +2,21 @@ import React from 'react';
 
 class SearchMovies extends React.Component {
 
-	searchMovies = async () => {
+	searchMovies = async (e) => {
+		e.preventDefault();
 		console.log("searching movies...")
+		const apiKey = '5dcf7f28a88be0edc01bbbde06f024ab'
+		const query = 'Jurassic Park'
+
+		const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
+
+		try {
+			const res = await fetch(url)
+			const data = await res.json()
+			console.log(data)
+		} catch (err) {
+			console.error(err)
+		}
 	}
 
 	render(){
