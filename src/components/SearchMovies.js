a7e783cd4d5daef90189025c4286b1977cbd6997
Alexandre Dias <alexbrigido@hotmail.com>
2020-05-28 09:34:21 -0300

    slyling

--- src/components/SearchMovies.js
+++ src/components/SearchMovies.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 class SearchMovies extends React.Component {
 
+	searchMovies = async () => {
+		console.log("searching movies...")
+	}
+
 	render(){
 		return (
-			<form className="form">
+			<form className="form" onSubmit={this.searchMovies}>
 				<label className="label" htmlFor="query">Movie Name</label>
 				<input className="input" type="text" name="query"
 						placeholder="i.e. Jurassic Park"/>
